6c62c0f61c7b27dc8ae37116429bd1c7f763854f
GABRIEL CHEDIEX VINER <46916327@ort>
2023-10-12 10:36:15 -0300

    Proyectos de 5to año

---
+++ TP01 Practica Node/src/modules/matematica.js
@@ -0,0 +1,11 @@
+const PI = 3.14;
+
+const suma =(num1,num2) =>{ return num1+num2}
+
+const resta =(num1,num2) =>{ return num1-num2}
+
+const dividir =(num1,num2) =>{ return num1/num2}
+
+const multiplicar =(num1,num2) =>{ return num1*num2}
+
+export{PI,suma,resta,dividir,multiplicar}
